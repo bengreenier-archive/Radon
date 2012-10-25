84d5c24f05a69721ee9d7582280ae9a4a202e5fe
B3nGr33ni3r <mygithub@b3nhub.com>
2012-10-24 22:51:55 -0400

    non object oriented functions added
    added types, schemes, and static functions to accomplish what the stream
    object does.

--- src/Main.cpp
+++ src/Main.cpp
@@ -51,7 +51,7 @@ public:
 
 	bool serialize(T data)
 	{
-		fwrite(&data,sizeof(T),sizeof(data),m_file);
+	   fwrite(&data,sizeof(T),sizeof(data),m_file);
 		if (0==ferror(m_file))
 			return true;
 		else
@@ -77,6 +77,102 @@ public:
 
 };
 
+
+
+//non object oriented versions of above
+template<class T>
+bool serialize(char* file, T data, Scheme scheme,Type type)
+{
+    FILE* m_file;
+     switch(scheme)
+    {
+
+        case bin:
+            switch(type)//the b is cause we're working with binary data
+            {
+            case rappend:
+                m_file = fopen(file,"rb+");
+                break;
+            case roverwrite:
+                m_file = fopen(file,"wb+");
+                break;
+            case read:
+                m_file = fopen(file,"rb");
+                break;
+            case append:
+                m_file = fopen(file,"ab");
+                break;
+            case overwrite:
+                m_file = fopen(file,"wb");
+                break;
+            }
+            break;
+    }
+
+    fwrite(&data,sizeof(T),sizeof(data),m_file);
+    if (0==ferror(m_file))
+    {
+        fclose(m_file);
+        return true;
+    }else{
+        fclose(m_file);
+        return false;
+    }
+
+
+}
+
+template<class T>
+T deserialize(char* file, Scheme scheme,Type type)
+{
+    FILE* m_file;
+	     switch(scheme)
+		{
+
+            case bin:
+                switch(type)//the b is cause we're working with binary data
+                {
+                case rappend:
+                    m_file = fopen(file,"rb+");
+                    break;
+                case roverwrite:
+                    m_file = fopen(file,"wb+");
+                    break;
+                case read:
+                    m_file = fopen(file,"rb");
+                    break;
+                case append:
+                    m_file = fopen(file,"ab");
+                    break;
+                case overwrite:
+                    m_file = fopen(file,"wb");
+                    break;
+                }
+                break;
+        }
+
+    rewind(m_file);
+    fseek (m_file , 0 , SEEK_END);
+    long lSize = ftell (m_file);
+    rewind (m_file);
+
+    T* result = new T();//(T) malloc(sizeof(T)*lSize);
+    fread(result,sizeof(T),lSize,m_file);
+
+    if (ferror(m_file)!=0)
+    {
+        fclose(m_file);
+        return -1;
+    }
+    else
+    {
+        fclose(m_file);
+        return *result;
+    }
+}
+
+
+
 };
 
 int main()
@@ -86,19 +182,26 @@ int main()
 	 */
 	int b = 1030404;
 
-	Radon::Stream<int> stream ((char*)"serialized.ser",Radon::bin);
+	Radon::Stream<int> stream ((char*)"serialized.ser",Radon::bin,Radon::roverwrite);
 
 	stream.serialize(b);
 
 	int b2 = stream.deserialize();
-	printf("%i should = %i (samestream)",b,b2);
+	printf("%i should = %i (samestream)\n",b,b2);
 	stream.destroy();
 
 
-	Radon::Stream<int> exstream ((char*)"serialized.ser",Radon::read);
+	Radon::Stream<int> exstream ((char*)"serialized.ser",Radon::bin,Radon::read);
 	int a = exstream.deserialize();
 	exstream.destroy();
 
-	printf("%i should = %i",a,b);
+	printf("%i should = %i\n",a,b);
+
+
+	float nonoop = 13245.678f;
+	Radon::serialize<float>((char*)"nonoop.ser",nonoop,Radon::bin,Radon::roverwrite);
+
+    float result = Radon::deserialize<float>((char*)"nonoop.ser",Radon::bin,Radon::read);
+    printf("%f should match %f (nonoop)\n",result,nonoop);
 
 }
